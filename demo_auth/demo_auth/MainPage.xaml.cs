c04cb351179baaf141ee16c3754c96d9af9a613d
prerna <prernakamra47@gmail.com>
2021-12-05 09:42:04 +0530

    login page

---
+++ demo_auth/demo_auth/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace demo_auth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new loggedPage();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
